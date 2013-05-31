549e9029acf34534767e668ebba93942bd9ab7db
Adam Wenham <adamwenham64@gmail.com>
2013-05-31 20:05:26 +0100

    init with pg because r4 and sqlite3 don't play well on windows

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleAppR4::Application.config.secret_key_base = '05b159ba13434debc9c59f4d925edc9132141967dfc9912e060fbb967702cc8815deec402e6a6d4b2e1ca3b165688e6b4f33d8b0da9a5d3a1cc8a370ab2d1282'
