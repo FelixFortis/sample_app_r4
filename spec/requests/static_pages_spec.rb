b555f91c22500df64de4919b49b8544412adead6
Adam Wenham <adamwenham64@gmail.com>
2013-06-05 08:22:21 +0100

    updated app layout to dry up code, added title yields

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -26,14 +26,14 @@ describe "StaticPages" do
   end
 
   describe "About page" do
-    it "should have the content 'About'" do
+    it "should have the content 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_content('About')
+      expect(page).to have_content('About Us')
     end
 
-    it "should have the title 'About'" do
+    it "should have the title 'About Us'" do
       visit '/static_pages/about'
-      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About')
+      expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
     end
   end
 end
